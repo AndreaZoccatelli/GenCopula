48e3071499269435291430b0af6a25180cec56e9
AndreaZoccatelli <azoccatellicomunicazione@gmail.com>
2023-07-07 23:16:51 +0200

    '

--- R/pipeline.R
+++ R/pipeline.R
@@ -4,8 +4,6 @@ pipeline = function(train, id, models = c("Logistic", "RandForest", "XGBoost"), 
   table_summary = avg_var_table(models, table)
   aug_table = augment_CV(DB@train_set, id, share, models, best_copula_i, outliers_r, nearest)
   aug_table_summary = avg_var_table(models, aug_table)
-  setClass("scores tables", representation(regular = "data.frame", reg_summary = "data.frame",
-                                           augmented = "data.frame", aug_summary = "data.frame"))
   tables = new("scores tables",regular = table, reg_summary = table_summary,
                augmented = aug_table, aug_summary = aug_table_summary)
   return(tables)
